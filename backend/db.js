5faf8765fe13ba203aa0c0ff8377cd5909188505
Puneetprashar <21MC3012@rgipt.ac.in>
2024-06-25 11:55:06 +0530

    handled env

--- backend/db.js
+++ backend/db.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
 
+const url = process.env.MONGODB_URL;
 mongoose.connect(url);
 const todoSchema = new mongoose.Schema({title:String,description:String,completed:Boolean});
 
